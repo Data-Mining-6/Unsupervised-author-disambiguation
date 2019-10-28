43872b1cb2b8af3ade373b51bad6354b18da2759
Anurag-Patil <cs17btech11004@iith.ac.in>
2019-10-28 14:50:29 +0530

    keywords added in features vector

--- data_mining.py
+++ data_mining.py
@@ -1,8 +1,12 @@
 import numpy as np
+from nltk.corpus import stopwords
 
 data_file_name = 'data_set_aminar_dblp.v11/dblp_papers_v11.txt'
 #data_file_name = 'data_set_aminar_dblp.v11/sample_of_dblp_papers_v11.txt'
 no_of_sample = 1000
+
+stop_words = set(stopwords.words('english'))
+unwanted_chars = ['-', '(', ')', '_', '\\', ',', '.', "'", '"', ':', ':', '%']
 
 def get_next_word(this_paper):
 	global j
@@ -74,6 +78,7 @@ class Paper:
 		self.url = []
 		self.abstract = ''
 		self.indexed_abstract = Indexed_abstract()
+		self.word_with_frequency = {}
 
 class Collection:
 	def __init__(self):
@@ -182,8 +187,9 @@ class Collection:
 							j = j + 1
 						paper.year = int(temp)
 					elif(key == 5):
-						while j < len(this_paper) and this_paper[j] != ']':
-							paper.keywords.append(get_next_word(this_paper))
+						if(this_paper[j + 3] != ']'):
+							while j < len(this_paper) and this_paper[j] != ']':
+								paper.keywords.append(get_next_word(this_paper))
 					elif(key == 6):
 						while j < len(this_paper) and this_paper[j + 1] != ']':
 							temp = Fos()
@@ -204,8 +210,9 @@ class Collection:
 							if temp.name not in paper.fos_list:
 								paper.fos_list.append(temp.name)
 					elif(key == 7):
-						while j < len(this_paper) and this_paper[j] != ']':
-							paper.references.append(get_next_word(this_paper))
+						if(this_paper[j + 3] != ']'):
+							while j < len(this_paper) and this_paper[j] != ']':
+								paper.references.append(get_next_word(this_paper))
 					elif(key == 8):
 						j = j + 2
 						temp = ''
@@ -223,6 +230,7 @@ class Collection:
 						paper.lang = get_next_word(this_paper)
 					elif(key == 13):
 						paper.publisher = get_next_word(this_paper)
+						paper.publisher = paper.publisher.lower()
 					elif(key == 14):
 						paper.volume = get_next_word(this_paper)
 					elif(key == 15):
@@ -252,6 +260,8 @@ class Collection:
 						while j < len(this_paper) and this_paper[j + 1] != '}':
 							temp1 = Word_index()
 							temp1.word = get_next_word(this_paper)
+							temp1.word = temp1.word.lower()
+							temp1.word = ''.join(i for i in temp1.word if not i in unwanted_chars)
 							j = j + 3
 							temp2 = ''
 							while j < len(this_paper) and this_paper[j] != ']':
@@ -264,6 +274,12 @@ class Collection:
 								j = j + 1
 							temp1.index.append(temp2)
 							paper.indexed_abstract.InvertedIndex.append(temp1)
+						for x in paper.indexed_abstract.InvertedIndex:
+							if x.word.isalpha() and (x.word not in stop_words):
+								if x.word not in paper.word_with_frequency:
+									paper.word_with_frequency[x.word] = len(x.index)
+								else:
+									paper.word_with_frequency[x.word] += len(x.index)
 			self.paper.append(paper)
 		input_file.close()
 
@@ -314,6 +330,10 @@ for x in name_set:
 		list_of_fos = []
 		list_of_years = []
 		list_of_references = []
+		list_of_lang = []
+		list_of_publishers = []
+		list_of_keywords = []
+		keywords_with_frequency = {}
 		print()
 		for y in name_set[x]:
 			for z in collection.paper[y[0]].authors_name_list:
@@ -321,47 +341,91 @@ for x in name_set:
 					list_of_co_authors.append(z)
 		for y in name_set[x]:
 			for z in collection.paper[y[0]].fos_list:
-				if(z not in list_of_fos):
+				if (z not in list_of_fos) and (z != ''):
 					list_of_fos.append(z)
 		for y in name_set[x]:
-			if collection.paper[y[0]].year not in list_of_years:
+			if (collection.paper[y[0]].year not in list_of_years) and (collection.paper[y[0]].year != 0):
 				list_of_years.append(collection.paper[y[0]].year)
 		for y in name_set[x]:
 			for z in collection.paper[y[0]].references:
-				if z not in list_of_references:
+				if (z not in list_of_references) and (z != ''):
 					list_of_references.append(z)
+		for y in name_set[x]:
+			if (collection.paper[y[0]].lang not in list_of_lang) and (collection.paper[y[0]].lang != ''):
+				list_of_lang.append(collection.paper[y[0]].lang)
+		for y in name_set[x]:
+			if (collection.paper[y[0]].publisher not in list_of_publishers) and (collection.paper[y[0]].publisher != ''):
+				list_of_publishers.append(collection.paper[y[0]].publisher)
+		for y in name_set[x]:
+			for z in collection.paper[y[0]].word_with_frequency:
+				if z not in keywords_with_frequency:
+					keywords_with_frequency[z] = collection.paper[y[0]].word_with_frequency[z]
+				else:
+					keywords_with_frequency[z] += collection.paper[y[0]].word_with_frequency[z]
+
+
+
+		for z in keywords_with_frequency:
+			if ((keywords_with_frequency[z] > 1) and (keywords_with_frequency[z] < (1.5 * len(name_set[x])))):
+				if (z not in list_of_keywords) and (z != ''):
+					list_of_keywords.append(z)
+		#print(keywords_with_frequency)
+
+		print("Co-authors : ", end = '')
 		print(list_of_co_authors)
+		print("fos : ", end = '')
 		print(list_of_fos)
+		print("years : ", end = '')
 		print(list_of_years)
+		print("references : ", end = '')
 		print(list_of_references)
+		print("lang : ", end = '')
+		print(list_of_lang)
+		print("publishers : ", end = '')
+		print(list_of_publishers)
+		print("keywords : ", end = '')
+		print(list_of_keywords)
 
 		feature_matrix = []
 		for y in name_set[x]:
 			feature_vector = []
 			for z in list_of_co_authors:
-				if z in collection.paper[y[0]].authors_name_list:
-					feature_vector.append(1)
-				else:
-					feature_vector.append(0)
+				feature_vector.append(collection.paper[y[0]].authors_name_list.count(z))
+
 			for z in list_of_fos:
-				if z in collection.paper[y[0]].fos_list:
-					feature_vector.append(1)
-				else:
-					feature_vector.append(0)
+				feature_vector.append(collection.paper[y[0]].fos_list.count(z))
+
 			for z in list_of_years:
 				if(z == collection.paper[y[0]].year):
 					feature_vector.append(1)
 				else:
 					feature_vector.append(0)
+				
 			for z in list_of_references:
-				if z in collection.paper[y[0]].references:
+				feature_vector.append(collection.paper[y[0]].references.count(z))
+
+			for z in list_of_lang:
+				if(z == collection.paper[y[0]].lang):
 					feature_vector.append(1)
 				else:
 					feature_vector.append(0)
+
+			for z in list_of_publishers:
+				if(z == collection.paper[y[0]].publisher):
+					feature_vector.append(1)
+				else:
+					feature_vector.append(0)
+
+			for z in list_of_keywords:
+				if z in collection.paper[y[0]].word_with_frequency:
+					feature_vector.append(collection.paper[y[0]].word_with_frequency[z])
+				else:
+					feature_vector.append(0)
+			
 			feature_matrix.append(feature_vector)
-			#print(feature_vector)
+			print(feature_vector)
 		feature_np_matrix = np.array(feature_matrix)
-		print(feature_np_matrix)
+		#print(feature_np_matrix)
 
 		#main algo
 		freq = np.sum(feature_np_matrix, axis = 0)
@@ -369,13 +433,16 @@ for x in name_set:
 		#print(freq)
 		#print(x_freq)
 		p_x_f = feature_np_matrix / freq[None, :]
+		#print("p_x_f")
 		#print(p_x_f)
 		p_f_x = feature_np_matrix / x_freq[:, None]
+		#print("p_f_x")
 		#print(p_f_x)
 		p_x_x = np.matmul(p_x_f, p_f_x.T)
-		#print(p_x_x)
+		print("p_x_x")
+		print(p_x_x)
 
-		l = 0.001
+		l = 0.1
 
 		while (len(name_set[x]) > 1):
 			#keep_cluster_id = {}
@@ -389,14 +456,15 @@ for x in name_set:
 			#print(c_sys_matrix)
 
 			cluster_vector = np.matmul(x_freq.T, c_sys_matrix)
-			print(cluster_vector.shape)
+			#print(cluster_vector.shape)
 
-			print(x_freq.shape)
+			#print(x_freq.shape)
 			p_x_c = np.multiply((np.matmul(x_freq.reshape(len(x_freq ), 1), (1 / cluster_vector).reshape(1, len(cluster_vector)))), c_sys_matrix)
 			#print(p_x_c)
 
 			p_c_c = np.matmul(c_sys_matrix.T, np.matmul(p_x_x, p_x_c))
-			#print(p_c_c)
+			print("p_c_c")
+			print(p_c_c)
 
 			merge = []
 			max = 0
@@ -417,12 +485,20 @@ for x in name_set:
 				for y in merge:
 					name_set[x][row_index].extend(name_set[x][column_index])
 					del name_set[x][column_index]
-			print(name_set[x])
+			#print(name_set[x])
 
 		count_a = count_a + len(name_set[x])
-		print(name_set[x])
+		#print(name_set[x])
 		print()
 	else:
 		count_a = count_a + 1
 print(count_a)
-print(name_set)
+
+for x in name_set:
+	if (len(name_set[x]) > 1):
+		print(x, end = " ")
+		print(name_set[x])
+	else:
+		if (len(name_set[x][0]) > 1):
+			print(x, end = " ")
+			print(name_set[x])
